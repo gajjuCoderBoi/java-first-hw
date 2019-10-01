2b08059ec1e96e05852e77a6afda7427db4aa5ad
Mohammad Javed <ghazanfar9131@gmail.com>
2019-09-30 22:16:43 -0400

    maxOfTwoNumbers() - Done

---
+++ src/com/cognizant/Main.java
@@ -0,0 +1,13 @@
+package com.cognizant;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m);
+    }
+
+    private int maxOfTwoNumbers(int n1, int n2){
+        return n1>n2 ? n1 : n2;
+    }
+}
